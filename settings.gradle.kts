b587562a5469cb086e7e10ac0c33ecbba1164a7e
真心 <ceo@iqianye.cn>
2021-10-07 21:13:20 +0800

    Mod: 暂时开坑 (没设备调试)

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "ZMusic"
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.icedlab.tech/loom")
+        maven("https://repository.hanbings.io/proxy")
+        maven("https://maven.fabricmc.net")
+        gradlePluginPortal()
+    }
+}
